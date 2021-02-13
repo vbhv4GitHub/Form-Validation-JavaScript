5e0ad1c9a0837b8feaa7d2012dcfbce46e02164c
vbhv4991GitHub <ravi9406126068@gmail.com>
2021-02-13 19:59:48 +0530

    Submit button feature almost working. Final touch still pending.

--- JS/app.js
+++ JS/app.js
@@ -3,6 +3,11 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 
+// Variable to be true while submission
+let validUserName = false;
+let validEmail = false;
+let validPhoneNumber = false;
+
 // Adding event listners
 username.addEventListener('blur', () => {
     // console.log("Name is blurred.");
@@ -10,11 +15,12 @@ username.addEventListener('blur', () => {
     let regex = /^[a-zA-Z]([0-9a-zA-Z]){2,14}$/;
     let str = username.value;
     console.log(regex, str);
-    if(regex.test(str)) {
+    if (regex.test(str)) {
         // console.log("It matched.");
         username.classList.remove('is-invalid');
+        validUserName = true;
     }
-    else{
+    else {
         // console.log("No match.");
         username.classList.add('is-invalid');
     }
@@ -26,10 +32,11 @@ email.addEventListener('blur', () => {
     let regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
     let str = email.value;
     console.log(regex, str);
-    if(regex.test(str)) {
+    if (regex.test(str)) {
         email.classList.remove('is-invalid');
+        validEmail = true;
     }
-    else{
+    else {
         email.classList.add('is-invalid');
     }
 });
@@ -38,11 +45,27 @@ phone.addEventListener('blur', () => {
     let regex = /^\d{10}$/;
     let str = phone.value;
     console.log(regex, str);
-    if(regex.test(str)) {
+    if (regex.test(str)) {
         phone.classList.remove('is-invalid');
+        validPhoneNumber = true;
     }
-    else{
+    else {
         phone.classList.add('is-invalid');
+    }
+});
+
+//Handling submit button
+let submit = document.getElementById('submit');
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (validEmail && validPhoneNumber && validUserName) {
+        let alert = document.getElementById('alert');
+        alert.classList.add("show");
+    }
+    else {
+        let alert2 = document.getElementById('alert2');
+        alert2.classList.add("show");
     }
 });
 
